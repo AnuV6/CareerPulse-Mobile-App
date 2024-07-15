542c5afbfcd211d5126e9d9672230605d79ecba7
Anupa <anupa@gmail.com>
2024-07-15 18:49:48 +0530

    Add app pages and widgets

---
+++ career_pulse/lib/pages/splash1.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Splash1 extends StatefulWidget {
+  const Splash1({super.key});
+
+  @override
+  State<Splash1> createState() => _Splash1State();
+}
+
+class _Splash1State extends State<Splash1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash Screen 1'),
+        backgroundColor: const Color.fromARGB(255, 29, 164, 5),
+      ),
+    );
+  }
+}
