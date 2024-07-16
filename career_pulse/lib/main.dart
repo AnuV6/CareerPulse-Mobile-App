6f5a7fbec7714851908aefa88c4e7d8d16b69164
DularaAbhiranda <ict21924@fot.sjp.ac.lk>
2024-07-16 20:57:22 +0530

    Main Ignore this

--- career_pulse/lib/main.dart
+++ career_pulse/lib/main.dart
@@ -1,13 +1,11 @@
-import 'package:career_pulse/pages/splash1.dart';
-//import ur page here
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:career_pulse/pages/splash1.dart'; // Import your splash1.dart page here
 
-void main() async{
-  runApp(const MainApp());
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -16,11 +14,13 @@ class MainApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Creer Pulse Anupa case',
+    return MaterialApp(
+      title: 'Career Pulse Anupa case',
       debugShowCheckedModeBanner: false,
-      home: Splash1(), 
+      theme: ThemeData(
+        primarySwatch: Colors.green, // Set your primary color
+      ),
+      home: const Splash1(), // Set your initial screen here
     );
   }
 }
-
