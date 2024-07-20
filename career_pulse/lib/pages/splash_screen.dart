e4793bff658cba923881b7e4e1b2a6e131680519
Anupa Dinuranga <112252294+AnuV6@users.noreply.github.com>
2024-07-20 22:05:51 +0530

    Revert "button and screen fix"

--- career_pulse/lib/pages/splash_screen.dart
+++ career_pulse/lib/pages/splash_screen.dart
@@ -1,42 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:career_pulse/stuffs/colors.dart';
-import 'package:career_pulse/pages/login.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
-
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _navigateToLogin();
-  }
-
-  _navigateToLogin() async {
-    await Future.delayed(const Duration(seconds: 3), () {});
-    Navigator.pushReplacement(
-      // ignore: use_build_context_synchronously
-      context,
-      MaterialPageRoute(builder: (context) => const LoginScreen()),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primaryColor,
+      backgroundColor: AppColors.primaryColor, 
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
-              'assets/logo.png',
+              'assets/logo.png', 
               height: 150,
             ),
             const SizedBox(height: 20),
@@ -58,8 +35,7 @@ class _SplashScreenState extends State<SplashScreen> {
             ),
             const SizedBox(height: 50),
             const CircularProgressIndicator(
-              valueColor:
-                  AlwaysStoppedAnimation<Color>(AppColors.secondaryColor),
+              valueColor: AlwaysStoppedAnimation<Color>(AppColors.secondaryColor),
             ),
           ],
         ),
