c7ccfd46376a37b6263d08c1fc5a1114cdbc3c62
DularaAbhiranda <ict21924@fot.sjp.ac.lk>
2024-07-21 23:53:24 +0530

    fix Yellow Box bug in Regi.

--- career_pulse/lib/pages/register.dart
+++ career_pulse/lib/pages/register.dart
@@ -29,159 +29,166 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.secondaryColor,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Create Account',
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-                color: AppColors.primaryColor,
-              ),
-            ),
-            const SizedBox(height: 32),
-
-            // Full Name
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Full Name',
-                labelStyle: TextStyle(color: AppColors.headingColor),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 10),
-
-            // Email
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                labelStyle: TextStyle(color: AppColors.headingColor),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 10),
-
-            // Password
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-                labelStyle: const TextStyle(color: AppColors.headingColor),
-                border: const OutlineInputBorder(),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _obscureText ? Icons.visibility_off : Icons.visibility,
-                    color: AppColors.headingColor,
-                  ),
-                  onPressed: () {
-                    _togglePasswordVisibility();
-                  },
+      body: SingleChildScrollView(
+        // Enables scrolling when keyboard appears
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                  height:
+                      80), // Increased space here to push everything downward
+              const Text(
+                'Create Account',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.primaryColor,
                 ),
               ),
-              obscureText: _obscureText,
-            ),
+              const SizedBox(
+                  height: 32), // Maintain space between title and first input
 
-            // Remember me button and Forget password
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Checkbox(
-                      value: _rememberMe,
-                      onChanged: (bool? value) {
-                        _toggleRememberMe(value);
-                      },
-                    ),
-                    const Text(
-                      'Remember me',
-                      style: TextStyle(color: AppColors.headingColor),
-                    ),
-                  ],
+              // Full Name
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'Full Name',
+                  labelStyle: TextStyle(color: AppColors.headingColor),
+                  border: OutlineInputBorder(),
                 ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/forgotPassword');
-                  },
-                  child: const Text(
-                    'Forgot Password?',
-                    style: TextStyle(color: AppColors.primaryColor),
+              ),
+              const SizedBox(height: 10),
+
+              // Email
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(color: AppColors.headingColor),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              // Password
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: const TextStyle(color: AppColors.headingColor),
+                  border: const OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility_off : Icons.visibility,
+                      color: AppColors.headingColor,
+                    ),
+                    onPressed: _togglePasswordVisibility,
                   ),
                 ),
-              ],
-            ),
-
-            const SizedBox(height: 32),
-
-            // Sign Up button
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: AppColors.buttonColor,
-                padding: const EdgeInsets.symmetric(vertical: 16),
+                obscureText: _obscureText,
               ),
-              child: const Center(
-                child: Text(
-                  'SIGN UP',
-                  style: TextStyle(color: Colors.white),
+
+              const SizedBox(height: 16),
+
+              // Remember me and Forget password
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: _rememberMe,
+                        onChanged: (bool? value) {
+                          _toggleRememberMe(value);
+                        },
+                      ),
+                      const Text(
+                        'Remember me',
+                        style: TextStyle(color: AppColors.headingColor),
+                      ),
+                    ],
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/forgotPassword');
+                    },
+                    child: const Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: AppColors.primaryColor),
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 32),
+
+              // Sign Up button
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.buttonColor,
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                ),
+                child: const Center(
+                  child: Text(
+                    'SIGN UP',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
-            ),
 
-            const SizedBox(height: 16),
+              const SizedBox(height: 16),
 
-            // Sign in with Google button
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: AppColors.buttonColor2,
-                padding: const EdgeInsets.symmetric(vertical: 16),
-              ),
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/google.png',
-                      height: 24,
-                    ),
-                    const SizedBox(width: 8),
-                    const Text(
-                      'SIGN UP WITH GOOGLE',
-                      style: TextStyle(color: AppColors.headingColor),
-                    ),
-                  ],
+              // Sign in with Google button
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.buttonColor2,
+                  padding: const EdgeInsets.symmetric(vertical: 16),
                 ),
-              ),
-            ),
-
-            const SizedBox(height: 32),
-
-            // Sign In button
-            Center(
-              child: TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/login');
-                },
-                child: RichText(
-                  text: const TextSpan(
-                    text: 'Already have an account? ',
-                    style: TextStyle(color: Color.fromARGB(255, 40, 20, 121)),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: 'Sign In',
-                        style: TextStyle(color: Colors.red),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/google.png',
+                        height: 24,
+                      ),
+                      const SizedBox(width: 8),
+                      const Text(
+                        'SIGN UP WITH GOOGLE',
+                        style: TextStyle(color: AppColors.headingColor),
                       ),
                     ],
                   ),
                 ),
               ),
-            ),
-          ],
+
+              const SizedBox(height: 32),
+
+              // Sign In button
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  child: RichText(
+                    text: const TextSpan(
+                      text: 'Already have an account? ',
+                      style: TextStyle(color: Color.fromARGB(255, 40, 20, 121)),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Sign In',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50), // Add some padding at the bottom
+            ],
+          ),
         ),
       ),
     );
