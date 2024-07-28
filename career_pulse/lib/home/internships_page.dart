9a74c7ad9aace714fa8f535cf37a231c0a827d12
DulajHansana <ict21819@fot.sjp.ac.lk>
2024-07-28 21:44:21 +0530

    Internship Screen

--- career_pulse/lib/home/internships_page.dart
+++ career_pulse/lib/home/internships_page.dart
@@ -1,9 +1,59 @@
-// internships_page.dart
 import 'package:flutter/material.dart';
 import 'package:career_pulse/widgets/AppBarWithBackButton.dart'; // Import CustomAppBar
 
 class InternshipsPage extends StatelessWidget {
   const InternshipsPage({super.key});
+
+  final List<Map<String, String>> internships = const [
+    {
+      'companyLogo': 'assets/ilogo4.png',
+      'title': 'Software Engineering',
+      'company': 'Vertusa Software Solution',
+      'role': 'Web Developer'
+    },
+    {
+      'companyLogo': 'assets/ilogo2.png',
+      'title': 'Software Engineering',
+      'company': '99X Software Solutions',
+      'role': 'Full-Stack Developer'
+    },
+    {
+      'companyLogo': 'assets/ilogo3.png',
+      'title': 'NetWork Engineering',
+      'company': 'Google',
+      'role': 'Network Administrator'
+    },
+    {
+      'companyLogo': 'assets/ilogo4.png',
+      'title': 'Audio Engineering',
+      'company': 'Vertusa Software Solution',
+      'role': 'Broadcast Technician'
+    },
+    {
+      'companyLogo': 'assets/ilogo2.png',
+      'title': 'Software Engineering',
+      'company': '99X Software Solutions',
+      'role': 'UX/UI Designer'
+    },
+    {
+      'companyLogo': 'assets/ilogo3.png',
+      'title': 'Software Engineering',
+      'company': 'Google',
+      'role': 'Fron-end Developer'
+    },
+    {
+      'companyLogo': 'assets/ilogo3.png',
+      'title': 'Software Engineering',
+      'company': 'Google',
+      'role': 'UX/UI Designer'
+    },
+    {
+      'companyLogo': 'assets/ilogo4.png',
+      'title': 'Network Engineering',
+      'company': 'Vertusa Software Solution',
+      'role': 'Network Architect'
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +62,48 @@ class InternshipsPage extends StatelessWidget {
         title: 'Internships',
         onBack: () => Navigator.of(context).pop(),
       ),
-      body: const Center(
-        child: Text('This is the Internships page.'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: internships.length,
+          itemBuilder: (context, index) {
+            final internship = internships[index];
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(internship['companyLogo']!),
+                  ),
+                  title: Text(internship['title']!),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(internship['company']!),
+                      Text(internship['role']!),
+                    ],
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.bookmark_border),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
