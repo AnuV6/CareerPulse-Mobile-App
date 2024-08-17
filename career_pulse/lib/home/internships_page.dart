954359c629f58e547194a6cb875f23a23683749d
NadunDaluwatta <nadundaluwatta26@gmail.com>
2024-08-17 12:08:41 +0530

    job popup screen updated

--- career_pulse/lib/home/internships_page.dart
+++ career_pulse/lib/home/internships_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-//test
 import 'package:career_pulse/widgets/AppBarWithBackButton.dart'; // Import CustomAppBar
-import 'package:career_pulse/widgets/internship_card.dart'; // Import CustomAppBar
+import 'package:career_pulse/widgets/internship_card.dart'; // Import InternshipCard
+import 'package:career_pulse/widgets/job_popup.dart'; // Import JobPopup
 
 class InternshipsPage extends StatelessWidget {
   const InternshipsPage({super.key});
@@ -11,50 +11,21 @@ class InternshipsPage extends StatelessWidget {
       'companyLogo': 'assets/ilogo4.png',
       'title': 'Software Engineering',
       'company': 'Vertusa Software Solution',
-      'role': 'Web Developer'
+      'role': 'Web Developer',
+      'location': 'Colombo, Sri Lanka.',
+      'date': '2024-08-13',
+      'daysAgo': '2 days ago'
     },
     {
       'companyLogo': 'assets/ilogo2.png',
       'title': 'Software Engineering',
       'company': '99X Software Solutions',
-      'role': 'Full-Stack Developer'
+      'role': 'Full-Stack Developer',
+      'location': 'Colombo, Sri Lanka.',
+      'date': '2024-08-11',
+      'daysAgo': '4 days ago'
     },
-    {
-      'companyLogo': 'assets/ilogo3.png',
-      'title': 'NetWork Engineering',
-      'company': 'Google',
-      'role': 'Network Administrator'
-    },
-    {
-      'companyLogo': 'assets/ilogo4.png',
-      'title': 'Audio Engineering',
-      'company': 'Vertusa Software Solution',
-      'role': 'Broadcast Technician'
-    },
-    {
-      'companyLogo': 'assets/ilogo2.png',
-      'title': 'Software Engineering',
-      'company': '99X Software Solutions',
-      'role': 'UX/UI Designer'
-    },
-    {
-      'companyLogo': 'assets/ilogo3.png',
-      'title': 'Software Engineering',
-      'company': 'Google',
-      'role': 'Front-end Developer'
-    },
-    {
-      'companyLogo': 'assets/ilogo3.png',
-      'title': 'Software Engineering',
-      'company': 'Google',
-      'role': 'UX/UI Designer'
-    },
-    {
-      'companyLogo': 'assets/ilogo4.png',
-      'title': 'Network Engineering',
-      'company': 'Vertusa Software Solution',
-      'role': 'Network Architect'
-    },
+    // Add other internships here with similar structure
   ];
 
   @override
@@ -74,14 +45,20 @@ class InternshipsPage extends StatelessWidget {
               title: internship['title']!,
               company: internship['company']!,
               role: internship['role']!,
+              location: internship['location']!, // Pass location
+              datePosted: internship['date']!, // Pass datePosted
+              daysAgo: internship['daysAgo']!, // Pass daysAgo
               onTap: () {
-                Navigator.pushNamed(
-                  context,
-                  '/internshipDetails',
-                  arguments: {
-                    'title': internship['title']!,
-                    'company': internship['company']!,
-                    'role': internship['role']!,
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return JobPopup(
+                      companyName: internship['company']!,
+                      roleTitle: internship['role']!,
+                      location: internship['location']!,
+                      datePosted: internship['date']!,
+                      daysAgo: internship['daysAgo']!,
+                    );
                   },
                 );
               },
